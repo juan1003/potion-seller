97e13443aa52202b163c7081b4c700d6c6ed505b
Juan De Jesus <jdejesus93@gmail.com>
2023-12-01 11:02:54 -0600

    Removing unused code

--- src/app/api/contact.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default async function POST(request: NextRequest) {
-    const { name, subject, description } = await request.json();
-    console.log(name, subject, description);
-    return NextResponse.json({ message: 'Thanks for submitting your issue.'});
-}
